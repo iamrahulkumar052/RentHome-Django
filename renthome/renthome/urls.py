c9e7efef20964d94c525fa6732447465ed229795
Rahul Kumar <159034627+iamrahulkumar052@users.noreply.github.com>
2024-08-09 09:09:19 +0530

    update

--- renthome/renthome/urls.py
+++ renthome/renthome/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('',include('renters.renters_urls')),
-    # path('user/',include('owners.owners_urls')),
     path('',include('accounts.accounts_urls')),
+    path('',include('renters.renters_urls')),
+    # path('user/',include('owners.owners_urls')),
 ]
